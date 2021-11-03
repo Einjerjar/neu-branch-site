14ee2bb67f9679c8b4830df6d33ba122b5a03aaa
Edward Jasper Flores <edward.flores@neu.edu.ph>
2021-11-03 15:46:54 +0800

    Added some changes on Events page ty

--- src/pages/Events.svelte
+++ src/pages/Events.svelte
@@ -2,6 +2,26 @@
 import Divider from "@/components/Divider.svelte";
 import NewsTicker from "@/components/NewsTicker.svelte";
 import Slider from "@/components/Slider.svelte";
+import Cards from "@/components/admission/Cards.svelte";
+
+//images for announcement
+let imgSrc= [
+    "https://neu.edu.ph/main/assets/images/posts_images/AntiBullying19.jpg",
+    "https://www.neu.edu.ph/main/assets/images/posts_images/44thArenaHS9.jpg",
+    "https://www.eaglenews.ph/wp-content/uploads/2017/04/NEU-graduates-480x320.jpg"
+  ];
+
+  let information = [
+    "This information is about the Basic Education Announcements",
+    "This information is about the SHS Announcements",
+    "This information is about the Undergraduate Announcements",
+  ];
+
+  let announcementType = [
+    {type: "Basic Education Announcement", id: 0},
+    {type: "Senior High School Announcement", id: 1},
+    {type: "Undergraduate Announcement", id: 2}
+  ];
 
 </script>
 <div>
@@ -18,6 +38,18 @@ import Slider from "@/components/Slider.svelte";
         <div class="bg-primary-900 text-white h-20 text-4xl uppercase flex items-center justify-center">
             <p>announcement</p>      
         </div>
-        <Slider/>
+
+        <div class="items-center flex flex-row justify-center">
+            {#each announcementType as at (at.id)}
+              <Cards imageSrc={imgSrc[at.id]} schoolType={at.type}>
+                <p class="text-center">{information[at.id]}</p>
+              </Cards>
+            {/each}
+          </div>
+
+          <Slider/>
+
+
     </div>
 </div>
+
