2a1d4e7a5ed6ef34277511489bc79d87582a5fb1
Edward Jasper Flores <edward.flores@neu.edu.ph>
2021-11-04 16:02:23 +0800

    Some changes on events page

--- src/pages/Events.svelte
+++ src/pages/Events.svelte
@@ -31,24 +31,128 @@ let imgSrc= [
         <Slider/>
         <Divider/>
         <div class="bg-primary-900 text-white h-20 text-4xl uppercase flex items-center justify-center">
-            <p>events</p>      
+            <p>announcement</p>
         </div>
         <Divider/>
         <Slider/>
         <Divider/>
         <div class="bg-primary-900 text-white h-20 text-4xl uppercase flex items-center justify-center">
-            <p>announcement</p>      
+            <p>events</p>
         </div>
+        <Divider></Divider>
+<!--Event Section Start-->
 
-        <div class="items-center flex flex-row justify-center">
-            {#each announcementType as at (at.id)}
-              <Cards imageSrc={imgSrc[at.id]} schoolType={at.type}>
-                <p class="text-center">{information[at.id]}</p>
-              </Cards>
-            {/each}
+        <div class="container event items-center flex flex-row justify-center">
+
+          <div class="row justify-content-center text-center">
+
+            <div class="offset-sm-1 col-sm-5 p-8">
+              <img src="https://www.neu.edu.ph/main/assets/images/posts_images/44thArenaHS9.jpg" class="picture-style" alt="">
+              <div class="event-content box-style">
+                <h4>Event 1</h4>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Expedita nostrum neque voluptatem?Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span><br>
+                  <a href="">join</a>
+              </div>
+            </div>
+
+            <div class="offset-sm-1 col-sm-5 p-8">
+              <img src="https://neu.edu.ph/main/assets/images/posts_images/AntiBullying19.jpg" class="picture-style" alt="">
+              <div class="event-content box-style">
+                <h4>Event 2</h4>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Expedita nostrum neque voluptatem?Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span><br>
+                  <a href="">join</a>
+              </div>
+            </div>
           </div>
-          <Slider/>
 
-    </div>
+          <div class="row justify-content-center text-center">
+
+            <div class="offset-sm-1 col-sm-5 p-8">
+              <img class="picture-style" src="https://www.eaglenews.ph/wp-content/uploads/2017/04/NEU-graduates-480x320.jpg"  alt="">
+              <div class="event-content box-style">
+                <h4>Event 3</h4>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Expedita nostrum neque voluptatem?Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span><br>
+                  <a href="">join</a>
+              </div>
+            </div>
+
+            <div class="offset-sm-1 col-sm-5 p-8">
+              <img src="https://neu.edu.ph/main/assets/images/posts_images/AntiBullying19.jpg" class="picture-style" alt="">
+              <div class="event-content box-style">
+                <h4>Event 4</h4>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Expedita nostrum neque voluptatem?Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span><br>
+                  <a href="">join</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Slider></Slider>
+      </div>
+
 </div>
 
+
+<!--EVENT SECTION START-->
+<style>
+.event{
+  margin: 200px auto;
+}
+
+.event-content{
+  box-shadow: 10px 10px 10px -10px rgba(0, 0, 0, 0.3),
+  inset 10px 10px 10px -10px
+  rgba(0, 0, 0, 0.5);
+  padding: 10px;
+  cursor: pointer;
+  background: #fff !important;
+  margin-top: -70px !important;
+  margin-left: 20px !important;
+  position: absolute;
+}
+
+.event-content h4{
+  font-weight: bold;
+  font-style: italic;
+  font-size: 20px;
+  margin-bottom: 5px;
+}
+
+.event-content span{
+  font-style: italic;
+  font-size: 15px;
+}
+
+.event-content a{
+  text-transform: uppercase;
+  font-weight: bold;
+  margin-top: 10px !important;
+}
+
+.event img{
+  transition: 0.6s;
+}
+
+.event img:hover{
+  transform: scale(1.1);
+  cursor: pointer;
+}
+.event .col-sm-5{
+  margin-bottom: 200px !important;
+}
+
+.picture-style{
+  height: 500px;
+  width: 500px;
+}
+
+.box-style{
+  height: 150px;
+  width: 500px;
+}
+
+
+</style>
