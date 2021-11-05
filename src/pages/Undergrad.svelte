1687d5f4bea7555cc91d7bfe33469ef4d4f31c73
Malchi Jade Roque <malachi.roque@neu.edu.ph>
2021-11-05 21:34:49 +0800

    Locking texts

--- src/pages/Undergrad.svelte
+++ src/pages/Undergrad.svelte
@@ -9,6 +9,9 @@
     </div>
 
     <h1><strong>WE ARE ERANIANS</strong></h1>
+
+    
+
     
     <p class="text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br> 
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took<br>
@@ -18,48 +21,59 @@
           publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     <hr class="bg-black my-15"/>
 
-    <h2 class="mb-5"><strong>Admission Procedure (NEU SHS Graduates)</strong></h2>
-    <h3 class="text-left ml-50"><strong>Step 1: Request for Temporary Student ID</strong></h3>
-    <p class="text-left ml-50 mr-100">Request for your Temporary Student ID thru sending the following details to admissionrequirements@neu.edu.ph</p>
-    <ul class="text-left ml-60 mb-10 list-disc">
-        <li>Name: (Last Name, First Name, Middle Name)</li>
-        <li>Course Applying: </li>
-        <li>Gender: </li>
-        <li>Year Graduated: </li>
-    </ul>
-
-    <h3 class="text-left ml-50"><strong>Step 2: Edit your personal sheet thru NEU Automate</strong></h3>
-    <p class="text-left ml-50 mr-100">Once you received the Temporary Username and Password thru email, You can now edt your
+    <div class="text-center">
+        <div class="text-left inline-block">
+            <h2 class="mb-5"><strong>Admission Procedure (NEU SHS Graduates)</strong></h2>
+            <h3><strong>Step 1: Request for Temporary Student ID</strong></h3>
+            <p>Request for your Temporary Student ID thru sending the following details to admissionrequirements@neu.edu.ph</p>
+            <ul class="mb-10 list-disc ml-10">
+                <li>Name: (Last Name, First Name, Middle Name)</li>
+                <li>Course Applying: </li>
+                <li>Gender: </li>
+                <li>Year Graduated: </li>
+            </ul>
+        
+            <h3><strong>Step 2: Edit your personal sheet thru NEU Automate</strong></h3>
+            <p>Once you received the Temporary Username and Password thru email, You can now edt your
                                 Personal Information Sheet thru this link NEU Automate for Application</p>
-    <ul class="text-left ml-60 mb-10 list-roman">
-        <li>Enter your log-in credentials (username and password)</li>
-        <li>Click Personal Info</li>
-        <li>Click Edit Personal Info </li>
-        <li>Fill out the General Student Personal Information Sheet (Special characters are not allowed)</li>
-        <li>CLick Proceed and Print Personal Info then Save/Print as PDF</li>
-    </ul>
+            <ul class="mb-10 list-roman ml-10">
+                <li>Enter your log-in credentials (username and password)</li>
+                <li>Click Personal Info</li>
+                <li>Click Edit Personal Info </li>
+                <li>Fill out the General Student Personal Information Sheet (Special characters are not allowed)</li>
+                <li>CLick Proceed and Print Personal Info then Save/Print as PDF</li>
+            </ul>
+        
 
-    <h3 class="text-left ml-50"><strong>Step 3: Submission</strong></h3>
-    <p class="text-left ml-50 mr-100">Submit all the following requirements thru this link Admission Requirements Submission Link
+            <h3><strong>Step 3: Submission</strong></h3>
+            <p>Submit all the following requirements thru this link Admission Requirements Submission Link
                                 Admission Requirements for Freshmen</p>
-    <ul class="text-left ml-60 list-disc">
-        <li>General Student Personal Information Sheet (GSPIS)</li>
-        <li>PSA Authenticated Birth Certificate</li>
-        <li>Senior High School Report Card (F-138) with Graduated Remarks</li>
-        <li>2x2 picture with white background</li>
-    </ul>
-    <p class="text-left ml-50">Admission Requirements for Transferees and Graduate School</p>
-    <ul class="text-left ml-60 mb-10 list-disc">
-        <li>General Student Personal Information Sheet (GSPIS)</li>
-        <li>PSA Authenticated Birth Certificate</li>
-        <li>Transcript of Records</li>
-        <li>Honorable Dismissal / Transfer Credential Certificate</li>
-        <li>2x2 picture with white background </li>
-    </ul> 
-    <h3 class="text-left ml-50"><strong>Step 4: Admission Approval</strong></h3>
-    <p class="text-left ml-50 mr-100 mb-20">Upon receipt of the advise from the College Admissions Office on the Approval of admission, you
-    may now log-in thru this link NEU Automate for Student Enrollment using the log-in credentials
-    (Username and Password) that you have created in filling out your General Information Sheet.</p>
+            <ul class="list-disc ml-10">
+                <li>General Student Personal Information Sheet (GSPIS)</li>
+                <li>PSA Authenticated Birth Certificate</li>
+                <li>Senior High School Report Card (F-138) with Graduated Remarks</li>
+                <li>2x2 picture with white background</li>
+            </ul>
+            <p>Admission Requirements for Transferees and Graduate School</p>
+            <ul class="mb-10 list-disc ml-10">
+                <li>General Student Personal Information Sheet (GSPIS)</li>
+                <li>PSA Authenticated Birth Certificate</li>
+                <li>Transcript of Records</li>
+                <li>Honorable Dismissal / Transfer Credential Certificate</li>
+                <li>2x2 picture with white background </li>
+            </ul> 
+        
+            <h3><strong>Step 4: Admission Approval</strong></h3>
+            <p class="max-w-screen-lg mb-30">Upon receipt of the advise from the College Admissions Office on the Approval of admission, you
+                may now log-in thru this link NEU Automate for Student Enrollment using the log-in credentials
+                (Username and Password) that you have created in filling out your General Information Sheet.
+            </p>
+        </div>
+        
+    </div>
+
+    
+
 </main>
 
 <style>
@@ -94,4 +108,7 @@
         font-family: 'Catamaran', sans-serif; 
         color: #791616;
     }
+    /* p{
+        max-width: 551px;
+    } */
 </style>
