6d425fcb55728d21ee8b7a32f122f93642fd5d2d
natchavez <nathanielbchavez@gmail.com>
2021-11-10 16:42:52 +0800

    Added components necessary for admission pages, stll need to make other fonts responsive

---
+++ src/components/admission/Banner.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let message = "Procedure";
+</script>
+
+
+<div class="page-home" style="font-family: 'Catamaran', sans-serif;">
+  <div class="bg-cover relative flex flex-wrap ">
+    <img class="w-full h-full" src="../public/images/admission_image.png" alt="neu_pic" >
+    <div class="mr-2 md:mr-10 sm:mr-7 absolute mx-auto centered right-0  max-w-sm text-lg md:text-6xl sm:text-3xl text-white text-right">{message}</div>
+  </div>
+</div>
+
+<style>
+    .centered {
+        /* left: 75%; */
+      top: 42%;
+    }
+
+</style>
